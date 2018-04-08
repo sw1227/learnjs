11dd74c0fcad2e3b49649dfb9c7b020387764980
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-04-08 14:52:20 +0900

    3: finish section3

--- public/app.js
+++ public/app.js
@@ -4,21 +4,116 @@
 // 名前衝突を避けるため、名前空間を作成する
 var learnjs = {}; // 実体はJSのオブジェクト
 
+// データモデルを定義する
+// ふつうのJSデータ構造でデータを保持すると、JSONドキュメントをDynamoDBでレコードとして格納
+// でき、オブジェクトマッピングの障壁が低くなる
+learnjs.problems = [
+    {
+	description: "What is truth?",
+	code: "function problem() { return __; }"
+    },
+    {
+	description: "Simple Math",
+	code: "function problem() { return 42 === 6 * __; }"
+    }
+];
+
+// データバインディング関数
+learnjs.applyObject = function(obj, elem) {
+    // 引数のJSオブジェクトのキーに基づいてjQueryオブジェクトの要素を更新
+    for (var key in obj) {
+	// HTML5のデータ属性に、どのプロパティをバインドするかが指定されている
+	elem.find('[data-name="' + key + '"]').text(obj[key]);
+    }
+};
+
+// 回答を再評価したことをユーザに知らせるエフェクト
+learnjs.flashElement = function(elem, content) {
+    elem.fadeOut('fast', function() {
+	elem.html(content);
+	elem.fadeIn();
+    });
+};
+
+// templateから指定したクラスの要素を取りだす
+learnjs.template = function(name) {
+    return $('.templates .' + name).clone();
+}
+
+learnjs.triggerEvent = function(name, args) {
+    $('.view-container>*').trigger(name, args);
+}
+
+// 正解時のFlash
+learnjs.buildCorrectFlash = function (problemNum) {
+    var correctFlash = learnjs.template('correct-flash');
+    var link = correctFlash.find('a');
+    if (problemNum < learnjs.problems.length) {
+	link.attr('href', '#problem-' + (problemNum + 1));
+    } else {
+	link.attr('href', '');
+	link.text("You're Finished!");
+    }
+    return correctFlash;
+};
 
 // Single Page Applicationは、#イベントによるルーティングによってリロードなしに移動する
 
 // ビュー関数: ビューに対応するjQueryオブジェクトを返す
-learnjs.problemView = function(problemNumber) {
-    return $('<div class="problem-view">').text('Problem #' + problemNumber + ' Coming soon!');
+learnjs.problemView = function(data) {
+    var problemNumber = parseInt(data, 10);
+    var view = learnjs.template('problem-view'); // templatesをコピーする
+    var problemData = learnjs.problems[problemNumber - 1];
+    var resultFlash = view.find('.result');
+
+    // textarea.answerに記入された内容で問題の__を置き換え、評価する
+    function checkAnswer() {
+	var answer = view.find('.answer').val();
+	var test = problemData.code.replace('__', answer) + '; problem();';
+	return eval(test);
+    }
+
+    function checkAnswerClick() {
+	if(checkAnswer()) {
+	    // 正解した時のみ次の問題へのリンクも表示
+	    learnjs.flashElement(resultFlash, learnjs.buildCorrectFlash(problemNumber));
+	} else {
+	    learnjs.flashElement(resultFlash, 'Incorrect!');
+	}
+	// falseを返すことでフォームを送信しないようにする
+	//  (送信するとページがリロードされてアプリケーションの状態がリセットされる)
+	return false;
+    }
+
+    view.find('.check-btn').click(checkAnswerClick); // クリックハンドラ
+    view.find('.title').text('Problem #' + problemNumber);
+    learnjs.applyObject(problemData, view); // データバインディング
+
+    if (problemNumber < learnjs.problems.length) {
+	var buttonItem = learnjs.template('skip-btn');
+	buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+	$('.nav-list').append(buttonItem);
+	// jQueryのbind()を使い、ルータ関数でtriggerされるremovingViewにリスナーをアタッチ
+	view.bind('removingView', function() {
+	    buttonItem.remove();
+	});
+    }
+
+    return view;
 }
 
+learnjs.landingView = function() {
+    return learnjs.template('landing-view');
+};
 
 // ルータ関数: URLハッシュ#とビュー関数を対応づける
 learnjs.showView = function(hash) {
 
     // URLハッシュ#とビュー関数を対応づけるルックアップテーブル
     var routes = {
-	'#problem': learnjs.problemView
+	'#problem': learnjs.problemView,
+	'': learnjs.landingView,
+	'#': learnjs.landingView
     };
 
     var hashParts = hash.split('-')
@@ -27,6 +122,7 @@ learnjs.showView = function(hash) {
 
     // ビュー関数を呼び出してview-containerに追加
     if (viewFn) {
+	learnjs.triggerEvent('removingView', []); // 既存のビューに削除されることを知らせる
 	$('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -3,22 +3,26 @@ describe('LearnJS', function() {
 	learnjs.showView('#problem-1');
 	expect($('.view-container .problem-view').length).toEqual(1);
     });
+
     it('shows the landing page view when there is no hash', function() {
 	// ヌルケース(デフォルトケース)
 	learnjs.showView('');
 	expect($('.view-container .landing-view').length).toEqual(1);
     });
+
     it('passes the hash view parameter to the view function', function() {
 	// JasmineのspyOn()を使い、指定の関数をスパイに置き換え、呼び出しを全て記録する
 	spyOn(learnjs, 'problemView');
 	learnjs.showView('#problem-42');
 	expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
+
     it('invokes the router when loaded', function() {
 	spyOn(learnjs, 'showView');
 	learnjs.appOnReady();
 	expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
+
     it('subscribe to the hash change event', function() {
 	// ロードした後にスパイすることで、hashchangeイベント時のshowView()の呼び出しを記録
 	learnjs.appOnReady();
@@ -29,10 +33,37 @@ describe('LearnJS', function() {
 	expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
     });
 
+
     describe('problem view', function() {
 	it('has a title that includes the problem Number', function () {
 	    var view = learnjs.problemView('1');
-	    expect(view.text()).toEqual('Problem #1 Coming soon!');
+	    // テキストのままだと, おそらくh3タグによって改行が入りfailする
+	    expect(view.find('.title').text()).toEqual('Problem #1');
+	});
+
+	it('shows the description', function() {
+	    var view = learnjs.problemView('1');
+	    expect(view.find('[data-name="description"]').text()).toEqual('What is truth?');
+	});
+
+	it('shows the problem code', function() {
+	    var view = learnjs.problemView('1');
+	    expect(view.find('[data-name="code"]').text())
+		.toEqual('function problem() { return __; }');
+	});
+
+	it('can check a correct answer by hitting a button', function() {
+	    var view = learnjs.problemView('1');
+	    view.find('.answer').val('true');
+	    view.find('.check-btn').click();
+	    expect(view.find('.result span').text()).toEqual('Correct!');
+	});
+
+	it('rejects an incorrect answer', function() {
+	    var view = learnjs.problemView('1');
+	    view.find('.answer').val('false');
+	    view.find('.check-btn').click();
+	    expect(view.find('.result').text()).toEqual('Incorrect!');
 	});
     });
 });
